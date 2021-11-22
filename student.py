2d57ba740ab625b11866740438ab2567f8f83aaa
daniff15 <dani.fig@ua.pt>
2021-11-22 05:08:55 +0000

    Functions for heuristics,holes and comp prob wrong

--- student.py
+++ student.py
@@ -41,7 +41,6 @@ class Student(SearchDomain):
         
                                      
     def satisfies(self, all_possibilities, stateGame):
-        #print("JOgo de agr" + str(stateGame))
         action_heuristic = {}
         for piece_action in all_possibilities:
             piece = piece_action[0]
@@ -51,7 +50,7 @@ class Student(SearchDomain):
 
             future_stateGame = stateGame["game"] + positions_piece
 
-            action_heuristic[piece_action[1]] = self.aggregate_height(future_stateGame)
+            action_heuristic[piece_action[1]] = self.heuristic(future_stateGame)
             #print("Futuro jogo ", future_stateGame)
         
         min_heuristic = ("a", action_heuristic["a"])
@@ -68,15 +67,69 @@ class Student(SearchDomain):
         for coord in state:
             if high_column[coord[0] - 1] < (30 - coord[1]):
                 high_column[coord[0] - 1] =  30 - coord[1]
-        print("Coluninhas ", high_column)
+        #print("Coluninhas ", high_column)
         return sum(high_column)
 
+    def bumpiness(self, state):
+        high_column = [0,0,0,0,0,0,0,0]
+        for coord in state:
+            if high_column[coord[0] - 1] < (30 - coord[1]):
+                high_column[coord[0] - 1] =  30 - coord[1]
+        bumpinessssss = 0
+        for i in range(len(high_column) - 1):
+            bumpinessssss += abs(high_column[i] - high_column[i+1])
+        return bumpinessssss
+    
+    def holes(self, state):
+        high_column = [0,0,0,0,0,0,0,0]
+        for coord in state:
+            if high_column[coord[0] - 1] < (30 - coord[1]):
+                high_column[coord[0] - 1] =  30 - coord[1]
+        
+        hole = 0
+        for max_column in high_column:
+            idx = 30 - max_column
+            i = 1
+            for coord in state:
+                if coord[0] == i:
+                    lista = [i, idx]
+                    idx += 1
+                    if lista not in state:
+                        hole += 1
+            i += 1
+        
+        return hole
+    
+    def completed_lines(self, state):
+        high_column = [0,0,0,0,0,0,0,0]
+        completed = 0
+        for coord in state:
+            if high_column[coord[0] - 1] < (30 - coord[1]):
+                high_column[coord[0] - 1] =  30 - coord[1]
+        
+        highest = max(high_column)
+
+        for coord in state:
+            idx = 30 - highest
+            i = 1
+            lista = [i , idx]
+            if lista in state:
+                i += 1
+            else:
+                idx +=1
+                i = 1
+            
+            if i == 8:
+                completed += 1 
+        return completed
+            
     def cost(self, state, action):
         pass
     
     # custo estimado de chegar de um estado a outro
-    def heuristic(self, state, goal):
-        pass
+    def heuristic(self, state):
+        #return self.aggregate_height(state) + self.bumpiness(state) + self.holes(state) + self.completed_lines(state)
+        return (self.aggregate_height(state) * -0.510066) + (self.bumpiness(state) * -0.184483) + (self.holes(state)* -0.35663) + (self.completed_lines(state) * 0.760666)
     
 
 async def agent_loop(server_address="localhost:8000", agent_name="student"):
